a3c19f47a222ddbded6cb516f52e59cb243dd5be
Jamal Cole <jamal@Jamals-MacBook-Pro.local>
2022-12-27 08:46:28 -0500

    frontend updates and mocking

---
+++ frontend/src/components/ButtonGroup/ButtonGroup.jsx
@@ -0,0 +1,118 @@
+import React, {Component} from "react";
+import "./ButtonGroup.scss";
+
+class ButtonGroup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lifecycle: "NOT_CLICKED"
+        }
+    }
+
+    deleteButtonClicked = () => {
+        this.setState({ lifecycle: "DELETE_BUTTON_CLICKED" });
+        console.log(this.state);
+    }
+
+    confirmButtonClicked = () => {
+        this.setState({ lifecycle: "CONFIRM_BUTTON_CLICKED" });
+        console.log(this.state);
+    }
+
+    submitButtonClicked = (msgType) => {
+        switch (msgType) {
+            case 'done': {
+                console.log('send web request: ', msgType);
+                // todo: send network call and update via websocket
+
+                this.setState({ lifecycle: "ALERT_SENT" });
+
+                break;
+            }
+
+            case 'delete': {
+                console.log('send web request: ', msgType);
+                break;
+            }
+
+            case 'picked_up': {
+                console.log('send web request: ', msgType);
+                break;
+            }
+
+            default: {
+                console.error(`unknown msgType ${msgType} for submitButtonClicked`);
+            }
+        }
+    }
+
+    cancelButtonClicked = (state) => {
+        switch (state) {
+            case 'ALERT_SENT': {
+                this.setState({ lifecycle: "DELETE_BUTTON_CLICKED" });
+                break;
+            }
+
+            case 'CONFIRM_BUTTON_CLICKED': {
+                this.setState({ lifecycle: "NOT_CLICKED" });
+                break;
+            }
+
+            case 'delete': {
+                console.log('send web request: ', state);
+                break;
+            }
+
+            default: {
+                console.error(`unknown currentState ${state} for cancelButtonClicked`);
+            }
+        }
+
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            (this.state.lifecycle === "CONFIRM_BUTTON_CLICKED") ? (
+                <div className="ButtonGroup">
+                    <button className="RedButton" onClick={this.cancelButtonClicked.bind(null, 'CONFIRM_BUTTON_CLICKED')}>
+                        Cancel
+                    </button>
+                    <button className="GreenButton" onClick={this.submitButtonClicked.bind(null, 'done')}>
+                        Send
+                    </button>
+                </div>
+            ) : ((this.state.lifecycle === "DELETE_BUTTON_CLICKED") ? (
+                    <div className="ButtonGroup">
+                        <button className="RedButton" onClick={this.cancelButtonClicked.bind(null, 'CONFIRM_BUTTON_CLICKED')}>
+                            Cancel
+                        </button>
+                        <button className="YellowButton" onClick={this.submitButtonClicked.bind(null, 'delete')}>
+                            Delete
+                        </button>
+                    </div>
+                ) : ((this.state.lifecycle === "ALERT_SENT") ? (
+                    <div className="ButtonGroup">
+                        <button className="YellowButton" onClick={this.cancelButtonClicked.bind(null, 'delete')}>
+                            Delete
+                        </button>
+                        <button className="OrangeButton" onClick={this.submitButtonClicked.bind(null, 'picked_up')}>
+                            Picked{'\u00A0'}Up
+                        </button>
+                    </div>
+                ) : (
+                <div className="ButtonGroupImage">
+                    <button>
+                        <img src="/check.png" alt="confirm button" onClick={this.confirmButtonClicked}/>
+                    </button>
+                    <button>
+                        <img src="/delete-button.png" alt="delete button" onClick={this.deleteButtonClicked}/>
+                    </button>
+                </div>
+                ))
+            )
+        )
+    }
+}
+
+export default ButtonGroup;
--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import "./Message.scss";
+import ButtonGroup from "../ButtonGroup/ButtonGroup";
 
 class Message extends Component {
     constructor(props) {
@@ -11,46 +12,17 @@ class Message extends Component {
         };
     }
 
-    deleteButtonClicked() {
-        console.log('delete button clicked!!')
-    }
-
-    confirmButtonClicked() {
-        console.log('confirm button clicked!!');
-    }
-
-    padText(str) {
-        const strLen = str.length;
-        if (strLen > 10) {
-            return str;
-        }
-
-        let pad = "";
-        for (let i=0; i < 10-strLen; i++) {
-            pad += "\u00A0";
-        }
-
-        return `${str}${pad}`
-    }
-
     render() {
         return (
             <div className="Message">
                 <div id="container">
                     <div className="containerText">
-                        {this.padText(this.state.message.body)}
+                        {this.state.message.body}
                     </div>
                     <div className="containerText">
                         856-875-7743
                     </div>
-                    <div id="buttonContainer">
-                        <button>
-                            <img src="/check.png" alt="confirm button" onClick={this.confirmButtonClicked}/>
-                        </button>
-                        <button>
-                            <img src="/delete-button.png" alt="delete button" onClick={this.deleteButtonClicked}/>
-                        </button>
-                    </div>
+                    <ButtonGroup />
                 </div>
             </div>
         )
